b1354c4b418de9ff9bf52f9fd3c8317586c559df
wanghongyu <1354998814@qq.com>
2023-10-05 09:25:09 +0800

    fix bug

--- hw12/basecc.cu
+++ hw12/basecc.cu
@@ -40,7 +40,6 @@ __global__ void _reluMaxPoll(type* input, type* output, int inputChannel, int in
     // printf("%d %d %d %d %d %d %d\n", inputChannel, inputRowSize, inputColSize,
     //     outputChannel, outputRowSize, outputColSize,
     //     kernelSize);
-    int threadNumPerBlock = blockDim.x * blockDim.y;
     int tid = threadIdx.x + threadIdx.y * blockDim.x;
     int row_boundary = (outputRowSize / blockDim.y - 1);
     int col_boundary = (outputColSize / blockDim.x - 1);
@@ -70,7 +69,7 @@ __global__ void _reluMaxPoll(type* input, type* output, int inputChannel, int in
     in_tile_thread_per_row = blockDim.x * 2 / trans_ele_num;
     in_tile_row_start = tid / in_tile_thread_per_row;
     in_tile_col = tid % in_tile_thread_per_row * trans_ele_num;
-    in_tile_row_stride = threadNumPerBlock / in_tile_thread_per_row;
+    in_tile_row_stride = blockDim.x * blockDim.y / in_tile_thread_per_row;
     #ifdef DEBUG
     if (threadIdx.x == 0 && threadIdx.y == 0) 
         printf("in_per_row = %d, in_tile_row_stride = %d, cur_in_block_height = %d ,cur_in_block_width = %d\n",
@@ -292,54 +291,50 @@ void reluMaxPool(float* d_input, int inputRowSize, int inputColSize, int inputCh
         
 template <
     const int BLOCK_HEIGHT, const int BLOCK_WIDTH, const int KERNEL_HEIGHT, const int KERNEL_WIDTH, const int MALLOC_TEMP_SIZE>
-__global__ void v1_convolution(element_type* in, element_type* out, element_type* kernel, element_type* kernelBias, int batch_size,
-    int inC, int inH, int inW,
-    int outC, int outH, int outW,
+__global__ void _conv2d(element_type* in, element_type* out, element_type* kernel, element_type* kernelBias, int batch_size,
+    int inputChannel, int inputRowSize, int inputColSize,
+    int outputChannel, int outputRowSize, int outputColSize,
     int kernelH, int kernelW)
 {
     // block id 与 thread id的读取与计算 分块是对target矩阵去分的
     // 目前按一个线程负责一个in的计算
-    int block_row = blockIdx.y;
-    int block_col = blockIdx.x;
-    int thread_row = threadIdx.y, thread_col = threadIdx.x;
-    int threadH = BLOCK_HEIGHT, threadW = BLOCK_WIDTH; // 线程网格范围
-    int thread_num_per_block = threadH * threadW, tid = thread_row * threadW + thread_col;
+    int thread_num_per_block = blockDim.x * blockDim.y, tid = threadIdx.y * blockDim.x + threadIdx.x;
     // 分块边界 boundary是限制正常范围 edge是需要特殊处理的范围
-    int row_boundary = outH / BLOCK_HEIGHT - 1,
-        col_boundary = outW / BLOCK_WIDTH - 1;
-    int row_edge = outH % BLOCK_HEIGHT, col_edge = outW % BLOCK_WIDTH;
+    int row_boundary = outputRowSize / BLOCK_HEIGHT - 1,
+        col_boundary = outputColSize / BLOCK_WIDTH - 1;
+    int row_edge = outputRowSize % BLOCK_HEIGHT, col_edge = outputColSize % BLOCK_WIDTH;
     // 线程计算值暂存区大小 动态分配不是很方便 需要在外部分配并带进来
     // 一般取单个计算元素和oc之积的2倍即可 因为block比较小
     const int temp_size = MALLOC_TEMP_SIZE;
 
     // if (tid==0)
-    //     printf("(%d %d)\n", block_row, block_col);
+    //     printf("(%d %d)\n", blockIdx.y, blockIdx.x);
 
-    /// 转移存储 GMEM --> SMEM
+    
     // __shared__ float s_in[BLOCK_HEIGHT + KERNEL_HEIGHT - 1][BLOCK_WIDTH + KERNEL_WIDTH - 1];
     __shared__ float s_kernel[1 + KERNEL_HEIGHT][1 + KERNEL_WIDTH]; // 开奇数内存会出错
     __shared__ float s_in[(BLOCK_HEIGHT + KERNEL_HEIGHT) * 2][(BLOCK_WIDTH + KERNEL_WIDTH) * 2];       // 要满足修正的尺寸
     float load_reg[4];
 
     // 当前block的起始位置
-    // int begin_pos = (block_row + thread_row) * BLOCK_HEIGHT + (block_col) * BLOCK_WIDTH + thread_col;
+    // int begin_pos = (blockIdx.y + threadIdx.y) * BLOCK_HEIGHT + (blockIdx.x) * BLOCK_WIDTH + threadIdx.x;
     //记录in矩阵的起始位置，他是根据in矩阵进行划分的
-    int begin_pos = block_row * BLOCK_HEIGHT * inW + block_col * BLOCK_WIDTH;
+    int begin_pos = blockIdx.y * blockDim.y * inputColSize + blockIdx.x * BLOCK_WIDTH;
 
     int single_trans_ele_num = 4;                               // 线程一次转移的数据数
     int cur_in_block_height = BLOCK_HEIGHT + KERNEL_HEIGHT - 1, // 读入in的block height，读入in的block的row大小
         cur_in_block_width = BLOCK_WIDTH + KERNEL_WIDTH - 1,    // 读入in的block width，读入in的block的col大小
-        in_tile_thread_per_row,                                 // 以tile为单位转移数据，一行需要的thread数
-        in_tile_row_start,                                      // tile的行起始位置
-        in_tile_col,                                            // tile的列
-        in_tile_row_stride;                                     // tile行跨度
+        in_tile_thread_per_row,                                 
+        in_tile_row_start,                                      
+        in_tile_col,                                            
+        in_tile_row_stride;                                    
 
     // 如果是in边缘的block，需要多读几个数据，相当于处理边界情况
-    if (block_row == row_boundary)
+    if (blockIdx.y == row_boundary)
     {
         cur_in_block_height = BLOCK_HEIGHT + row_edge + kernelH - 1;
     }
-    if (block_col == col_boundary)
+    if (blockIdx.x == col_boundary)
     {
         cur_in_block_width = BLOCK_WIDTH + col_edge + kernelW - 1;
     }
@@ -350,16 +345,15 @@ __global__ void v1_convolution(element_type* in, element_type* out, element_type
     in_tile_row_stride = thread_num_per_block / in_tile_thread_per_row; // 每个thread需要跳跃的大小
 
     // 下方都是读取第一个channel的数据
-    // 
     for (int i = 0; i < cur_in_block_height && in_tile_row_start < cur_in_block_height;
         i += in_tile_row_stride)
     {
-        // if (block_row == 0 && block_col == 0)
+        // if (blockIdx.y == 0 && blockIdx.x == 0)
         // {
         //     printf("%d (%d %d) %d %d\n", tid, in_tile_row_start + i, in_tile_col, cur_in_block_height, cur_in_block_width);
         // }
         FETCH_FLOAT4(load_reg[0]) =
-            FETCH_FLOAT4(in[begin_pos + OFFSET(in_tile_row_start + i, in_tile_col, inW)]);
+            FETCH_FLOAT4(in[begin_pos + OFFSET(in_tile_row_start + i, in_tile_col, inputColSize)]);
         s_in[in_tile_row_start + i][in_tile_col] = load_reg[0];
         s_in[in_tile_row_start + i][in_tile_col + 1] = load_reg[1];
         s_in[in_tile_row_start + i][in_tile_col + 2] = load_reg[2];
@@ -369,195 +363,150 @@ __global__ void v1_convolution(element_type* in, element_type* out, element_type
         {
             for (int j = in_tile_col + 1 * single_trans_ele_num; j < cur_in_block_width; j++)
             {
-                s_in[in_tile_row_start + i][j] = in[begin_pos + OFFSET(in_tile_row_start + i, j, inW)];
+                s_in[in_tile_row_start + i][j] = in[begin_pos + OFFSET(in_tile_row_start + i, j, inputColSize)];
             }
         }
     }
 
    
-    if (thread_row >= 0 && thread_row < KERNEL_HEIGHT && thread_col == 0)
+    if ( threadIdx.y < KERNEL_HEIGHT && threadIdx.x == 0)
     {
         for (int j = 0; j < KERNEL_WIDTH; j++)
         {
-            s_kernel[thread_row][j] = kernel[OFFSET(thread_row, j, KERNEL_WIDTH)];
+            s_kernel[threadIdx.y][j] = kernel[OFFSET(threadIdx.y, j, KERNEL_WIDTH)];
         }
     }
 
     __syncthreads();
    
-    // if (block_row == 0 && block_col == 0 && thread_row == 0 && thread_col == 0) // 16 8
-    // {
-    //     for (int i = 0; i < cur_in_block_height; i++)
-    //     {
-    //         for (int j = 0; j < cur_in_block_width; j++)
-    //         {
-    //             printf("(%d %d) %.2f|%.2f\n", i, j, s_in[i][j], in[begin_pos + OFFSET(i, j, inW)]);
-    //         }
-    //     }
-    // }
-    // if (block_row == 2 && block_col == 2 && tid == 0)
-    // {
-    //     for (int i = 0; i < KERNEL_HEIGHT; i++)
-    //     {
-    //         for (int j = 0; j < KERNEL_WIDTH; j++)
-    //         {
-    //             printf("(%d %d) %.2f|%.2f\n", i, j, s_kernel[i][j], kernel[OFFSET(thread_row, j, KERNEL_WIDTH)]);
-    //         }
-    //     }
-    // }
+
 
     // 逐个channel计算 一个线程负责block中的一个元素计算
-    // 修正out block的大小
-    int cur_out_block_height = BLOCK_HEIGHT, // 输出block height
-        cur_out_block_width = BLOCK_WIDTH,   // 输出block width
-        single_calculate_num = 1,            // 线程一次负责计算的元素数目
-        out_tile_thread_per_row,             // block按tile划分需要的线程数目
-        out_tile_row_start,                  // tile的行起始位置
-        out_tile_col,                        // tile的列起始位置
-        out_tile_row_stride;                 // tile行跨度
-    if (block_row == row_boundary)
+ 
+    int cur_out_block_height = blockDim.y,
+        cur_out_block_width = blockDim.x,  
+        out_tile_thread_per_row,             
+        out_tile_row_start,                  
+        out_tile_col,                       
+        out_tile_row_stride;                 
+    if (blockIdx.y == row_boundary)
     {
         cur_out_block_height = BLOCK_HEIGHT + row_edge;
     }
-    if (block_col == col_boundary)
+    if (blockIdx.x == col_boundary)
     {
         cur_out_block_width = BLOCK_WIDTH + col_edge;
     }
 
-    out_tile_thread_per_row = cur_out_block_width / single_calculate_num;
+    out_tile_thread_per_row = cur_out_block_width ;
     out_tile_row_start = tid / out_tile_thread_per_row;
-    out_tile_col = tid % out_tile_thread_per_row * single_calculate_num;
+    out_tile_col = tid % out_tile_thread_per_row ;
     out_tile_row_stride = thread_num_per_block / out_tile_thread_per_row;
 
-    float tmp[temp_size]; // 存储累积和 避免多次读取GMEM
+    float tmp[temp_size]; 
     for (int i = 0; i < temp_size; i++)
         tmp[i] = 0;
 
     int out_pos, temp_pos;
 
-    for (int oc = 0; oc < outC; oc++)
+    for (int oc = 0; oc < outputChannel; oc++)
     {
-        for (int ic = 0; ic < inC; ic++)
+        for (int ic = 0; ic < inputChannel; ic++)
         {
             // i,j 是相当于当前block起始位置而言
-            // 每个线程负责一个tile，元素数>线程数会进行轮替，会有少部分的重叠区域，代价不大（只要width不大）
             // 用ic的每个block去对oc的kernel进行计算
             for (int i = 0; i < cur_out_block_height && (out_tile_row_start + i) < cur_out_block_height;
                 i += out_tile_row_stride)
             {
-                for (int j = 0; j < single_calculate_num; j++)
+                
+                // 计算线程负责的元素 同一个oc的缓存顺序排列
+                // 不同oc偏移一个cur_out_block_height / out_tile_row_stride + 1的位置
+                temp_pos = i / out_tile_row_stride +
+                    oc * (cur_out_block_height / out_tile_row_stride + 1);
+                for (int ii = 0; ii < KERNEL_HEIGHT; ii++)
                 {
-                    // 计算线程负责的元素 同一个oc的缓存顺序排列
-                    // 不同oc偏移一个cur_out_block_height / out_tile_row_stride + 1的位置
-                    temp_pos = i / out_tile_row_stride + j +
-                        oc * (cur_out_block_height / out_tile_row_stride + 1);
-                    for (int ii = 0; ii < KERNEL_HEIGHT; ii++)
+                    for (int jj = 0; jj < KERNEL_WIDTH; jj++) // 更换的是SMEM中的内容，相对位置不变
                     {
-                        for (int jj = 0; jj < KERNEL_WIDTH; jj++) // 更换的是SMEM中的内容，相对位置不变
-                        {
-                            tmp[temp_pos] += s_in[out_tile_row_start + i + ii][out_tile_col + j + jj] * s_kernel[ii][jj];
-                        }
+                        tmp[temp_pos] += s_in[out_tile_row_start + i + ii][out_tile_col + jj] * s_kernel[ii][jj];
                     }
                 }
+                
             }
             // 读取下一个in channel和对应kernel的数据
-            if (ic + 1 < inC)
+            if (ic + 1 < inputChannel)
             {
                 for (int i = 0; i < cur_in_block_height && in_tile_row_start < cur_in_block_height;
                     i += in_tile_row_stride)
                 {
                     FETCH_FLOAT4(load_reg[0]) =
-                        FETCH_FLOAT4(in[begin_pos + (ic + 1) * inH * inW + OFFSET(in_tile_row_start + i, in_tile_col, inW)]);
+                        FETCH_FLOAT4(in[begin_pos + (ic + 1) * inputRowSize * inputColSize + OFFSET(in_tile_row_start + i, in_tile_col, inputColSize)]);
                     s_in[in_tile_row_start + i][in_tile_col] = load_reg[0];
                     s_in[in_tile_row_start + i][in_tile_col + 1] = load_reg[1];
                     s_in[in_tile_row_start + i][in_tile_col + 2] = load_reg[2];
                     s_in[in_tile_row_start + i][in_tile_col + 3] = load_reg[3];
                     if (in_tile_col + 2 * single_trans_ele_num > cur_in_block_width &&
-                        cur_in_block_width > in_tile_col + 1 * single_trans_ele_num) // 余量不足一次转移数
+                        in_tile_col + 1 * single_trans_ele_num < cur_in_block_width)
                     {
                         for (int j = in_tile_col + 1 * single_trans_ele_num; j < cur_in_block_width; j++)
                         {
-                            s_in[in_tile_row_start + i][j] = in[begin_pos + (ic + 1) * inH * inW + OFFSET(in_tile_row_start + i, j, inW)];
+                            s_in[in_tile_row_start + i][j] = in[begin_pos + (ic + 1) * inputRowSize * inputColSize + OFFSET(in_tile_row_start + i, j, inputColSize)];
                         }
                     }
                 }
-                if (thread_row >= 0 && thread_row < KERNEL_HEIGHT && thread_col == 0)
+                if ( threadIdx.y < KERNEL_HEIGHT && threadIdx.x == 0)
                 {
                     for (int j = 0; j < KERNEL_WIDTH; j++)
                     {
-                        s_kernel[thread_row][j] = kernel[(oc * inC + ic + 1) * kernelH * kernelW + OFFSET(thread_row, j, KERNEL_WIDTH)];
+                        s_kernel[threadIdx.y][j] = kernel[(oc * inputChannel + ic + 1) * kernelH * kernelW + OFFSET(threadIdx.y, j, KERNEL_WIDTH)];
                     }
                 }
             }
 
             __syncthreads();
-            // 验证数据转移
-            // if (ic + 1 < inC)
-            //     if (block_row == 2 && block_col == 2 && thread_row == 0 && thread_col == 0) // 16 8
-            //     {
-            //         for (int i = 0; i < cur_in_block_height; i++)
-            //         {
-            //             for (int j = 0; j < cur_in_block_width; j++)
-            //             {
-            //                 printf("(%d %d) %.2f|%.2f\n", i, j, s_in[i][j], in[begin_pos + (ic + 1) * inH * inW + OFFSET(i, j, inW)]);
-            //             }
-            //         }
-            //     }
+
         }
         // 读取下一个kernel channel数据
-        if (oc + 1 < outC)
+        if (oc + 1 < outputChannel)
         {
-            if (thread_row >= 0 && thread_row < KERNEL_HEIGHT && thread_col == 0)
+            if ( threadIdx.y < KERNEL_HEIGHT && threadIdx.x == 0)
             {
                 for (int j = 0; j < KERNEL_WIDTH; j++)
                 {
-                    s_kernel[thread_row][j] = kernel[(oc + 1) * inC * kernelH * kernelW + OFFSET(thread_row, j, KERNEL_WIDTH)];
+                    s_kernel[threadIdx.y][j] = kernel[(oc + 1) * inputChannel * kernelH * kernelW + OFFSET(threadIdx.y, j, KERNEL_WIDTH)];
                 }
             }
         }
         __syncthreads();
         // 写回 利用线程id计算写回位置
-        int i = 0, j = 0;
+        int i = 0;
         while (i < cur_out_block_height && (out_tile_row_start + i) < cur_out_block_height)
         {
-            while (j < single_calculate_num)
-            {
-                out_pos = oc * outH * outW +
-                    block_row * BLOCK_HEIGHT * outW + block_col * BLOCK_WIDTH +
-                    OFFSET(out_tile_row_start + i, out_tile_col + j, outW);
-                temp_pos = i / out_tile_row_stride + j +
+                out_pos = oc * outputRowSize * outputColSize +
+                    blockIdx.y * BLOCK_HEIGHT * outputColSize + blockIdx.x * BLOCK_WIDTH +
+                    OFFSET(out_tile_row_start + i, out_tile_col, outputColSize);
+                temp_pos = i / out_tile_row_stride +
                     oc * (cur_out_block_height / out_tile_row_stride + 1);
-                // if (tid == 0 && block_row == 0 && block_col == 0)
-                // {
-                //     printf("%d %d-(%d %d) %d %d\n", i, j, out_tile_row_start + i, out_tile_col + j,
-                //            temp_pos, out_pos);
-                // }
                 out[out_pos] = tmp[temp_pos] + kernelBias[oc];
-                j++;
-            }
+            
             i += out_tile_row_stride;
-            j = 0;
         }
         // 读取下一个in channel数据
         for (int i = 0; i < cur_in_block_height && in_tile_row_start < cur_in_block_height;
             i += in_tile_row_stride)
         {
-            // if (block_row == 0 && block_col == 0)
-            // {
-            //     printf("%d (%d %d) %d %d\n", tid, in_tile_row_start + i, in_tile_col, cur_in_block_height, cur_in_block_width);
-            // }
+
             FETCH_FLOAT4(load_reg[0]) =
-                FETCH_FLOAT4(in[begin_pos + OFFSET(in_tile_row_start + i, in_tile_col, inW)]);
+                FETCH_FLOAT4(in[begin_pos + OFFSET(in_tile_row_start + i, in_tile_col, inputColSize)]);
             s_in[in_tile_row_start + i][in_tile_col] = load_reg[0];
             s_in[in_tile_row_start + i][in_tile_col + 1] = load_reg[1];
             s_in[in_tile_row_start + i][in_tile_col + 2] = load_reg[2];
             s_in[in_tile_row_start + i][in_tile_col + 3] = load_reg[3];
             if (in_tile_col + 2 * single_trans_ele_num > cur_in_block_width &&
-                cur_in_block_width > in_tile_col + 1 * single_trans_ele_num) // 余量不足一次转移数
+                cur_in_block_width > in_tile_col + 1 * single_trans_ele_num) 
             {
                 for (int j = in_tile_col + 1 * single_trans_ele_num; j < cur_in_block_width; j++)
                 {
-                    s_in[in_tile_row_start + i][j] = in[begin_pos + OFFSET(in_tile_row_start + i, j, inW)];
+                    s_in[in_tile_row_start + i][j] = in[begin_pos + OFFSET(in_tile_row_start + i, j, inputColSize)];
                 }
             }
         }
@@ -585,7 +534,7 @@ void conv2d(float*  d_input, int inputRowSize, int inputColSize, int inputChanne
     // checkCudaErrors(cudaMemcpy(d_kernel, kernel.data(), kernelSize, cudaMemcpyHostToDevice));
     // checkCudaErrors(cudaMemcpy(d_kernelBias, kernelBias.data(), kernelBiasSize, cudaMemcpyHostToDevice));
 
-    v1_convolution<BLOCK_HEIGHT, BLOCK_WIDTH, KERNEL_HEIGHT, KERNEL_WIDTH, MALLOC_TEMP_SIZE>
+    _conv2d<BLOCK_HEIGHT, BLOCK_WIDTH, KERNEL_HEIGHT, KERNEL_WIDTH, MALLOC_TEMP_SIZE>
         << <dimGrid, dimBlock, 0, stream >> > (d_input, d_output, d_kernel, d_kernelBias,
             N, inputChannel, inputRowSize, inputColSize, outputChannel, outputRowSize, outputColSize, kernelRowSize, kernelColSize);
     // cudaDeviceSynchronize();
@@ -1063,7 +1012,7 @@ int main(int argc, char* argv[]) {
     int *d_predict, *d_labels;
     int *predict = (int*)malloc(sizeof(int) * labels.size());
     
-    const int nStreams = 2; 
+    const int nStreams = 1; 
     cudaStream_t streams[nStreams];
     for (int i = 0; i < nStreams; i++) {
         cudaStreamCreate(&streams[i]);
